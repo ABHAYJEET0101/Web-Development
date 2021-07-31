460ca4101108fac3a345b20cf84f4b54c7ff2734
Kunwar Abhayjeet Aaryan <vasuchdy.123@gmail.com>
2021-07-31 14:22:13 +0530

    added new

---
+++ lect-3/organiser/main.js
@@ -0,0 +1,18 @@
+let inputArr = process.argv.slice(2);
+console.log(inputArr);
+
+// node main.js tree "directoryPath"
+//node main.js organise "directoryPath"
+//node main.js help
+
+let command = inputArr[0];
+switch (command){
+    case "tree":
+        break;
+    case "organise":
+        break;
+    case "help":
+        break;
+    default:
+        
+}
