d45d4ee590f6343f34032ee566da9991eedbb869
Kunwar Abhayjeet Aaryan <vasuchdy.123@gmail.com>
2021-08-01 09:32:29 +0530

    new added

--- lect-4/work/script.js
+++ lect-4/work/script.js
@@ -1,8 +1,21 @@
 let request = require("request");
 let fs = require("fs");
+let cheerio = require("cheerio");
+let loadedHtml;
 
-request("http://www.google.com", function(err, res, body){
-    if(err){
-        fs.writeFileSync("index.html", body);
-    }
-})
+function responseHandler(err, res, body){
+    if(!err) {
+        loadedHtml = cheerio.load(body);
+        let allComments = loadedHtml(".cb-com-ln.ng-binding.ng-scope.cb-col.cb-col-90");
+        let topcomment = loadedHtml(allComments[0]);
+        let comment = topcomment.text()
+        console.log(comment); 
+    };
+}
+
+
+
+request(
+    "", 
+    responseHandler
+);
